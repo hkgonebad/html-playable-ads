771f41bfd0f007ffb4a7d02dda18e390cc22bc50
hkgonebad <harry007007@gmail.com>
2025-04-10 00:21:27 +0530

    fix: Update base URL in Vite configuration for deployment consistency

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/html-playable-ads/",
+  base: "/html-playable/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
